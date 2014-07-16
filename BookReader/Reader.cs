6b2cabb760f48c218c72b826a3e8264614a2fe5f
pk <karthi.email@gmail.com>
2014-07-16 13:05:32 +1000

    books sample api and client

---
+++ BookReader/Reader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace BookApiClient
+{
+    public class BookClient
+    {
+        public string BaseUri { get; set; }
+
+        public BookClient(string baseUri = null)
+        {
+            BaseUri = baseUri ?? "http://book.readify.com/api";
+        }
+
+        private readonly JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        public IEnumerable<dynamic> GetBook()
+        {
+            var client = new HttpClient {BaseAddress = new Uri(BaseUri)};
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/books");
+            request.Headers.Add("Accept", "application/json");
+
+            var response = client.SendAsync(request);
+
+            var content = response.Result.Content.ReadAsStringAsync().Result;
+            var status = response.Result.StatusCode;
+
+            if (status == HttpStatusCode.OK)
+            {
+                return !String.IsNullOrEmpty(content) ?
+                    JsonConvert.DeserializeObject<IEnumerable<dynamic>>(content, _jsonSettings)
+                    : new List<dynamic>();
+            }
+
+            throw new Exception("Server responded with a non 200 status code");
+        }
+    }
+}
---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BooksApi.Controllers
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
+    [RoutePrefix("api/books")]
+    public class BooksController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<Book> Get()
+        {
+            return new List<Book>
+            {
+                new Book {ISBN = "asdfads", Author = "k", Name = "m"},
+                new Book {ISBN = "asf", Author = "k", Name = "m"}
+            };
+        }
+
+        [Route("{id:int}")]
+        public Book Get(int id)
+        {
+            return new Book {ISBN = "asdfads", Author = "k", Name = "m"};
+        }
+    }
+}
